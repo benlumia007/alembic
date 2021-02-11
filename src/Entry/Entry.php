8190796b0a31d63a7fa33343fd550487327bb292
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-02-11 11:35:24 -0800

    change the post uri to

--- src/Entry/Entry.php
+++ src/Entry/Entry.php
@@ -188,20 +188,13 @@ class Entry {
 				array_shift( $parts );
 			}
 			$uri = join( '', $parts );
-		//	$uri = preg_replace( "/\d{4}-\d{2}-\d{2}./", '', $this->pathinfo['filename'] );
 		}
 
 		if ( $this->type ) {
 
 			if ( 'post' === $this->type->name() ) {
 
-				$timestamp = is_numeric( $this->meta( 'date' ) )
-					     ? $this->meta( 'date' )
-					     : strtotime( $this->meta( 'date' ) );
-
-				return $this->type->uri(
-					date( 'Y/m/d', $timestamp ) . '/' . $uri
-				);
+				return $this->type->uri( 'blog/' . $uri );
 			}
 
 			return $this->type->uri( $uri );
