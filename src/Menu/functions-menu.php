1628c4e96f12f55a763cc4691e1221981880ea2a
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-07-12 09:34:36 -0700

    update menus for primary and social menu

--- src/Menu/functions-menu.php
+++ src/Menu/functions-menu.php
@@ -1,13 +1,13 @@
 <?php
 
-function primary() {
+function primary_menu() {
 	$data = Benlumia007\Alembic\Config\File::get_instance()->get_data();
 	foreach ($data['primary'] as $name => $title ) { ?>
 		<li class="menu-item"><a href="<?= e( uri( $title['url'] ) ) ?>"><?= e( $title['title'] ); ?></a></li>
 	<?php }
 }
 
-function social() {
+function social_menu() {
 	$data = Benlumia007\Alembic\Config\File::get_instance()->get_data();
 	foreach ($data['social'] as $name => $title ) { ?>
 		<li class="menu-item"><a href="<?= e( $title['url'] ); ?>" target="_blank"><svg><?php include( public_path() . e( $title['svg'] ) ); ?></svg></a></li>
