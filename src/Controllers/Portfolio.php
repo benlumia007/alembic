e1ce97ffc4f88930d36be1fb53d11758d75a73ad
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-07-13 08:39:44 -0700

    add portfolio

---
+++ src/Controllers/Portfolio.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Benlumia007\Alembic\Controllers;
+
+use Benlumia007\Alembic\Entry\Entries;
+use Benlumia007\Alembic\Entry\Locator;
+use Benlumia007\Alembic\Engine;
+use Benlumia007\Alembic\App;
+
+class Portfolio {
+    protected $slug;
+    protected $path = '_portfolio';
+
+    protected $type;
+    protected $params = [];
+
+    public function __invoke( array $params = [] ) {
+        $this->params = ( array ) $params;
+
+        $this->slug = $this->params['name'];
+
+        $this->type = App::resolve( 'content/type')->get( 'portfolio' );
+
+        $entries = $this->entries();
+
+        $all = $entries->all();
+		$entry = array_shift( $all );
+
+        Engine::view( 'portfolio', [], [
+			'title'   => $entry ? $entry->title() : 'Not Found',
+			'query'   => $entry ? $entry : false,
+			'page'    => 1,
+			'entries' => $entries
+		] )->display();
+    }
+
+    protected function entries() {
+        $locator = new Locator( $this->type->path() );
+
+		return new Entries( $locator, [ 'slug' => $this->slug ] );
+    }
+}
