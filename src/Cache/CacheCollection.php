055052a4c1d3f23a81d6ea3789d4c17cbd65b4a7
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-07-22 23:19:03 -0700

    updated

--- src/Cache/CacheCollection.php
+++ src/Cache/CacheCollection.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * Cache Collection.
+ *
+ * @package   Alembic Core
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright Copyright 2021. Benjamin Lu
+ * @link      https://github.com/benlumia007/alembic-core
+ * @license   http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ */
+
 
 namespace Benlumia007\Alembic\Cache;
 
